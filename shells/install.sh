fd9bda92b93d9ef1b1ddd7a5d545cc5f984ffbc6
shihabict <shihabuddin.ict@gmail.com>
2025-01-28 11:49:41 -0600

    Update the requirements and install shell file

--- shells/install.sh
+++ shells/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt install python3-pip
+sudo apt install python3-venv
 python3 -m venv rlenv
 source rlenv/bin/activate
 
@@ -6,3 +8,4 @@ pip install wheel setuptools pip --upgrade
 pip3 install wheel setuptools pip --upgrade
 
 pip install -r requirements.txt
+pip install "gymnasium[classic-control]"
